95218f78005cd0ffae34dcaaec7a53d5d64a26be
Faique Saeed <shayankhan480@gmail.com>
2020-01-03 16:09:28 +0500

    Updates

---
+++ precache-manifest.851ecdb17e05791edede88f15f291770.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86d80e3dd9236fd7d3b51a6eabf67ad3",
+    "url": "/react-firestore/index.html"
+  },
+  {
+    "revision": "6d565c8106e40ba3e6ce",
+    "url": "/react-firestore/static/css/main.80b514db.chunk.css"
+  },
+  {
+    "revision": "1eec8056c87c115bce71",
+    "url": "/react-firestore/static/js/2.76ba9ff9.chunk.js"
+  },
+  {
+    "revision": "6d565c8106e40ba3e6ce",
+    "url": "/react-firestore/static/js/main.3c8f2a53.chunk.js"
+  },
+  {
+    "revision": "cd9156e0200336c4a73f",
+    "url": "/react-firestore/static/js/runtime~main.032ddae3.js"
+  }
+]);
